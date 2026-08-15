dd771e7f37ca33033fcc4dbaf347128c824202aa
Prateek Das <2025da04014@wilp.bits-pilani.ac.in>
2026-08-15 21:11:37 +0530

    Remove unused imports

--- model/train_models.py
+++ model/train_models.py
@@ -1,13 +1,11 @@
 #base load
 import pandas as pd
-import numpy as np
 import joblib
 import os
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
